17a4b1ca934a10dbf1eb341bd7ad3332fdd65834
simoelabd <mohamed.ouledelabd@emsi-edu.ma>
2025-12-13 10:26:54 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,28 @@
+Test de l’ajout d’un étudiant (POST)
+
+![TP6-T1.png](images/TP6-T1.png)
+
+Test de la récupération de tous les étudiants (GET)
+
+![TP6-T2.png](images/TP6-T2.png)
+
+Test de la suppression d’un étudiant (DELETE)
+
+![TP6-T3.png](images/TP6-T3.png)
+
+Test du comptage des étudiants (GET)
+
+![TP6-T4.png](images/TP6-T4.png)
+
+Test du nombre d’étudiants par année de naissance (GET)
+
+![TP6-T5.png](images/TP6-T5.png)
+
+Interface Swagger
+
+![TP6-Swager-Interface.png](images/TP6-Swager-Interface.png)
+
+Les tests unitaires
+
+![Tests Unitaires.png](images/Tests%20Unitaires.png)
+
---
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -0,0 +1,60 @@
+package com.example.studentmanagement.controller;
+
+import com.example.studentmanagement.entity.Student;
+import com.example.studentmanagement.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello from New Test :)";
+    }
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Student> save(@RequestBody Student student) {
+        Student savedStudent = studentService.save(student);
+        return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") int id) {
+        boolean deleted = studentService.delete(id);
+        if (deleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Operation(summary = "Récupérer tous les étudiants")
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> findAll() {
+        List<Student> students = studentService.findAll();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> countStudent() {
+        long count = studentService.countStudents();
+        return new ResponseEntity<>(count, HttpStatus.OK);
+    }
+
+    @GetMapping("/byYear")
+    public ResponseEntity<Collection<?>> findByYear() {
+        Collection<?> studentsByYear = studentService.findNbrStudentByYear();
+        return new ResponseEntity<>(studentsByYear, HttpStatus.OK);
+    }
+}
